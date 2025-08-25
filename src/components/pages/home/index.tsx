d2f9c16cc793980cc2aa207489f8000f50f6923d
bbosmedia <abbos.nurgulshanov@mail.ru>
2025-08-25 20:47:16 +0500

    ar hero added

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -8,12 +8,13 @@ import PresentationTwo from "./_components/presentation-two";
 import PresentationThree from "./_components/presentation-three";
 import HomeHero from "./_components/home-hero";
 import { useTranslation } from "react-i18next";
+import HomeArHero from "./_components/home-ar-hero";
 
 const HomePage = () => {
 	const { i18n } = useTranslation();
 	return (
 		<>
-			{i18n.language === "ar" ? <HomeHero /> : <HomeHero />}
+			{i18n.language === "ar" ? <HomeArHero /> : <HomeHero />}
 			<CoreFeatures />
 			<PresentationOne />
 			<PresentationTwo />
